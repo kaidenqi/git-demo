60caa880462e3fe060439e07ead4e4e53b3181ff
Kai Qi <55521333+kaidenqi@users.noreply.github.com>
2021-06-24 09:40:03 -0700

    added many files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import ExpenseItem from "./components/ExpenseItem.js";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started with react!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
